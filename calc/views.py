addd45fdff51f387245fadeef2b9404944596765
Nikesh Singh Bundelkhand <nikeshsingh@bundelkhand.github.com>
2020-05-22 00:15:43 +0200

    first commit

---
+++ calc/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def home(request): 
+    return render(request, 'home.html', {'name': 'Naveen'})
+
+
+def addition(request):
+
+    val1 = int(request.POST['num1'])
+    val2 = int(request.POST['num2'])
+
+    res = val1 + val2
+
+    return render(request, 'result.html', {'result': res})
