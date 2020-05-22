6de362973b490dbfa4476852b2a12b5b216765e3
Nikesh Singh Bundelkhand <nikeshsingh@bundelkhand.github.com>
2020-05-22 09:49:05 +0200

    Changed URL.py

--- Django_Web_Project/urls.py
+++ Django_Web_Project/urls.py
@@ -19,4 +19,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('Travello.urls')),
+
+    #I am changing to push these changes to github repository
 ]
